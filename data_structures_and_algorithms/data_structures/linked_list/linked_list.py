e9a7ff33cf22c0c522fb14d65fdbb037ae86abda
RaniaAbdullahh <raniaabdullah6600@gmail.com>
2020-12-05 19:59:25 +0200

    finished

--- data_structures_and_algorithms/data_structures/linked_list/linked_list.py
+++ data_structures_and_algorithms/data_structures/linked_list/linked_list.py
@@ -1,7 +1,62 @@
-class LinkedList:
-    """
-    Put docstring here
-    """
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
 
-    # put your LinkedList implementation here
+class LinkedList :
+    def __init__(self):
+        self.head=None
+        self.size=0
+
+    def insert(self,value):
+        node=Node(value)
+        if self.head == None:
+            self.head = node
+        else:
+            node.next=self.head
+            self.head=node
+        self.size+=1      
+
+
+    def includes (self,value):
+        current=self.head
+        while current and current.next != None:
+            if current.value == value:
+                return True
+            current=current.next
+        return False
+
+
+    def __str__(self):
+        current=self.head
+        result=''
+
+        if current == None:
+            return 'Empty List'
+        else:
+            while current:
+                result += f'{{{current.value}}}->'
+                current=current.next   
+            result += f'NULL'
+        return result      
+
+
+
+
+
+
+if __name__ == "__main__":
     pass
+    # brothers = LinkedList()
+    # node1=Node('rania')
+    # node2=Node('Moh')
+    # node2.next=node1
+    # brothers.insert('rania')
+    # brothers.insert('moh')
+    # brothers.insert('Malk')
+    # brothers.insert('aml')
+    # print(brothers)
+    # if brothers.includes('rania'): 
+    #     print ('True') 
+    # else: 
+    #     print ('false')
--- tests/data_structures/test_linked_list.py
+++ tests/data_structures/test_linked_list.py
@@ -1,8 +1,43 @@
-from data_structures_and_algorithms.data_structures.linked_list.linked_list import (
-    LinkedList,
-)
+from data_structures_and_algorithms.data_structures.linked_list.linked_list import LinkedList
+import pytest
 
 
-def test_instance():
+
+def test_instantiate_linked_list():
+    """
+    empty linked list
+    """
     ll = LinkedList()
-    assert isinstance(ll, LinkedList)
+    
+    assert ll.head == None
+
+
+def test_str(prep_ll):
+   
+    assert prep_ll.__str__() == '{Moh}->{Rania}->NULL'
+
+def test_incloud_value(prep_ll):
+    """"
+     Will return true when finding a value within the linked list that exists
+    """
+  
+    assert prep_ll.includes('Moh')
+    
+
+def test_incloud_false_value(prep_ll):
+    """"
+     Will return False when finding a value within the linked list that not exists
+    """
+   
+    assert prep_ll.includes('aml')==False
+    
+
+
+@pytest.fixture
+def prep_ll():
+    ll = LinkedList()
+    ll.insert('Rania')
+    ll.insert('Moh')
+    return ll
+
+
